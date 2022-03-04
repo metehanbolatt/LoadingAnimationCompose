4ba08eed2d232331ca34125c1918be1b7dcf462f
Metehan BOLAT <73827610+metehanie@users.noreply.github.com>
2022-03-04 14:55:21 +0300

    Circles ready.

---
+++ app/src/main/java/com/metehanbolat/loadinganimationcompose/LoadingAnimation.kt
@@ -0,0 +1,56 @@
+package com.metehanbolat.loadinganimationcompose
+
+import androidx.compose.animation.core.*
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
+
+@Composable
+fun LoadingAnimation(
+    modifier: Modifier = Modifier,
+    circleSize: Dp = 25.dp,
+    circleColor: Color = MaterialTheme.colors.primary,
+    spaceBetween: Dp = 10.dp,
+    travelDistance: Dp = 20.dp
+) {
+    val circles = listOf(
+        remember { Animatable(initialValue = 0f) },
+        remember { Animatable(initialValue = 0f) },
+        remember { Animatable(initialValue = 0f) }
+    )
+
+    val circleValues = circles.map { it.value }
+    val distance = with(LocalDensity.current) { travelDistance.toPx() }
+    val lastCircle = circleValues.size - 1
+
+    Row(modifier = modifier) {
+        circleValues.forEachIndexed { index, value ->
+            Box(
+                modifier = Modifier
+                    .size(circleSize)
+                    .graphicsLayer {
+                        translationY = -value * distance
+                    }
+                    .background(
+                        color = circleColor,
+                        shape = CircleShape
+                    )
+            )
+
+            if (index != lastCircle) {
+                Spacer(modifier = Modifier.width(spaceBetween))
+            }
+        }
+    }
+}
--- app/src/main/java/com/metehanbolat/loadinganimationcompose/MainActivity.kt
+++ app/src/main/java/com/metehanbolat/loadinganimationcompose/MainActivity.kt
@@ -3,6 +3,11 @@ package com.metehanbolat.loadinganimationcompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import com.metehanbolat.loadinganimationcompose.ui.theme.LoadingAnimationComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -10,7 +15,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             LoadingAnimationComposeTheme {
-
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    LoadingAnimation()
+                }
             }
         }
     }
